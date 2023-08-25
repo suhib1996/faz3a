02b12c40b1ab46a4fe709c8ae591f8c732076f0d
suhib1996 <141364772+suhib1996@users.noreply.github.com>
2023-08-25 10:13:00 +0300

    5

---
+++ secrets.php
@@ -0,0 +1,6 @@
+<?php
+// Keep your Stripe API key protected by including it as an environment variable
+// or in a private script that does not publicly expose the source code.
+
+// This is your test secret API key.
+$stripeSecretKey = 'sk_test_51NhxJRLZwL2jyxO6OS3huetX14xEfTd5o7WFct8O4s4uPnFyepgcln49mDvd3negNNwKpog4Ta7SGabLb6FkEKVM00OxVqrJhW';
